ce509fec093f98ae567b58f9fc1d074985cd5835
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-21 11:28:05 +0500

    add HW4_1

---
+++ HW4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два 
+// числа (A и B) и возводит число A в натуральную степень B.
+int a = Prompt("Введите число A => ");
+int b = Prompt("Введите число B => ");
+int level=Level(a, b);
+Console.WriteLine(level);
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int Level (int a, int b)
+{
+    int pow=1;
+    for (int i = 0; i < b; i++) pow *= a;
+    return pow;
+}
+
+
