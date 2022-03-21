a6af2869f77eb1f5044681df22e043cf84c74c67
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-21 20:19:06 +0500

    add Task4_2

---
+++ HW4_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+Console.Write("Введите число => ");
+string a =Console.ReadLine();
+int[] arr = new int[a.Length];
+int sum = 0;
+for (int i = 0; i < a.Length; i++)
+{
+    arr[i] = int.Parse(a[i].ToString());
+    sum+=arr[i];
+}    
+Console.WriteLine(sum);
